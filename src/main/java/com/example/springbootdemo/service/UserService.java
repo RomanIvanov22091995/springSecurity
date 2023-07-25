d327607db4a2b62e81c00e2a7115c01bdcdf3dbd
Roman <Roman22091995@yandex.ru>
2023-07-25 17:41:16 +0300

    update(push all)

---
+++ src/main/java/com/example/springbootdemo/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.service;
+
+import com.example.springbootdemo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public User findById(Long id);
+    public List<User> findAll();
+    public User saveUser (User user);
+    public void deleteById(Long id);
+}
